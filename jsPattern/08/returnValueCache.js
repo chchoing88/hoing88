3eb207b2a939f71dbe26417122171e33c60e4c94
merlin.ho <merlin.ho@kakaocorp.com>
2018-04-11 02:02:49 +0900

    패턴

--- jsPattern/08/returnValueCache.js
+++ jsPattern/08/returnValueCache.js
@@ -3,27 +3,27 @@
  */
 var Aspects = Aspects || {};
 
-
-Aspects.returnValueCache = function(sharedCache){
+// 해당 인자에 대한 키값을 가지고 리턴값을 캐싱한다. 
+Aspects.returnValueCache = function (sharedCache) {
   'use strict';
 
   var cache = sharedCache || {};
 
   return {
-    advice: function(targetInfo){
+    advice: function (targetInfo) {
 
       // 함수에 넘긴 인자를 캐시 키로 이용한다..
       // ( 객체 참조값 비교가 아닌, 문자열 비교를 하귀 위해 문자열로 바꾼다..)
 
       var cacheKey = JSON.stringify(targetInfo.args);
 
-      if(cache.hasOwnProperty(cacheKey)){
+      if (cache.hasOwnProperty(cacheKey)) {
         return cache[cacheKey];
       }
 
       // 장식된 함수를 가져와 실행한뒤
       // 그 반환값을 캐슁 저장한다..
-      var returnValue = Aop.next(targetInfo);
+      var returnValue = Aop.next(targetInfo); // 원본함수
       cache[cacheKey] = returnValue;
       return returnValue;
     }
--- jsPattern/10/Presentation.js
+++ jsPattern/10/Presentation.js
@@ -1,26 +1,35 @@
 /**
  * Created by merlin.ho on 2017. 6. 11..
  */
+// 10장. 팩토리 패턴
+// 팩토리는 객체를 단순히 찍어내는 함수다. 
+// Object.create 메서드 역시 언어 자체에 포함된 팩토리다.
+// 모듈은 데이터 감춤이라는 부가 기능이 있지만, 객체 생성/반환이 주 임무라는 점에서 
+// 엄밀히 말하면 팩토리다.
+
+// 팩토리를 사용하는 이유는 '제어' 와 '추상화' 를 강화하기 위해서다.
+// 여기서 '제어'와 '추상화'는 무엇인가..
+
 
 var Conference = Conference || {};
 
-Conference.Presentation = function(title , presenter){
+Conference.Presentation = function (title, presenter) {
   'use strict';
 
   // 에러 처리..
   // 1. new로 호출하지 않으면 에러..
   // 2. title이 없으면 에러.
 
-  if(!(this instanceof Conference.Presentation)){
+  if (!(this instanceof Conference.Presentation)) {
     throw new Error(Conference.Presentation.messages.mustUseNew);
   }
 
-  if(!title){
+  if (!title) {
     throw new Error(Conference.Presentation.messages.titleRequired);
   }
 };
 
 Conference.Presentation.messages = {
-  mustUseNew : 'Presentation은 반드시 "new"로 생성해야 합니다.',
-  titleRequired : 'title은 필수 입력 항목입니다.'
+  mustUseNew: 'Presentation은 반드시 "new"로 생성해야 합니다.',
+  titleRequired: 'title은 필수 입력 항목입니다.'
 };
